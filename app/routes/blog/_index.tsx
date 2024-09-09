1d13fb49c1b53199cf66a36620990e0db93b79ec
Miroslava Hreskova <hroncovam@yahoo.com>
2024-09-09 11:33:07 +0100

    Blog index updated

--- app/routes/blog/_index.tsx
+++ app/routes/blog/_index.tsx
@@ -10,9 +10,13 @@ export const meta: MetaFunction = () => {
 export default function Index() {
     return (
         <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-            Section Title: Thoughts & Paint Splatters
+          <h1>Section Title: Thoughts & Paint Splatters</h1>
+          <p>
             Write short entries about your experiences, challenges, and triumphs in your artistic journey.
+          </p>
+          <p>
             You could even post a mix of serious reflections and lighthearted updates like: The paintwater cup has been spilled again. Moment of silence for the carpet.
+          </p>
         </div>
     );
 }
