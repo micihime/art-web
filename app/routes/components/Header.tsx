c09c3e00e1729aea0d0eaa303dca5d19560bef0b
Miroslava Hreskova <hroncovam@yahoo.com>
2024-09-13 12:24:49 +0100

    Removing Contact

--- app/routes/components/Header.tsx
+++ app/routes/components/Header.tsx
@@ -11,13 +11,10 @@ export default function Header() {
             <a href="/about">About</a>
           </li>
           <li>
-            <a href="/progress-gallery">Gallery</a>
-          </li>
-          <li>
             <a href="/blog">Blog</a>
           </li>
           <li>
-            <a href="/contact">Contact</a>
+            <a href="/progress-gallery">Gallery</a>
           </li>
         </ul>
       </div>
