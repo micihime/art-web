656899d5bd677a8fe3874a665d4a54bba00276bc
Miroslava Hreskova <hroncovam@yahoo.com>
2024-09-13 11:43:33 +0100

    Updating Home

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -16,29 +16,12 @@ export default function Index() {
         <p>
           Hi, I&apos;m Miči, an artist-in-the-making who&apos;s just starting to paint outside the lines! 
           Join me as I splash colors and slowly figure out how to make them look like actual art. 
-          Spoiler alert: It&apos;s messy, but it&apos;s fun!
+        </p>
+        <p>
+          <strong>Spoiler alert:</strong> It&apos;s messy, but it&apos;s fun!
         </p>
         <blockquote>Mistakes? I call them &apos;happy accidents&apos; —thanks, Bob Ross!</blockquote>
       </div>
-      <div>
-        <a href="/about">
-          Learn more <span>About me</span>
-        </a>
-        <a href="/progress-gallery">
-          See my <span>Creations</span>
-        </a>
-        <a href="/blog">
-          Check out my <span>Blog</span>
-        </a>
-        <a href="/contact">
-          You can reach out <span>Here</span>
-        </a>
-      </div>
-      <footer>
-        Footer
-        Keep it simple, with links to your social media profiles (if any).
-        A funny closing line, like: Thanks for visiting! Now go paint something... or at least pretend to!
-      </footer>
     </>
   );
 }
