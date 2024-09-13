1e62bbe084ee7f76e377ab9fe7d525ed937c7b27
Miroslava Hreskova <hroncovam@yahoo.com>
2024-09-13 11:43:05 +0100

    Adding new component ArticleFooter

---
+++ app/routes/components/ArticleFooter.tsx
@@ -0,0 +1,21 @@
+export default function Index() {
+    return (
+        <footer className="post-footer">
+			<h3>About the author</h3>
+			<div className="byline">
+				<a href="https://mitchie.netlify.app/" className="photo">
+					<img src="/img/profile.jpg" height="120" width="120" alt="author" />
+				</a>
+				<h3><a href="https://mitchie.netlify.app/">Admin Admin</a></h3>
+				<div className="bio">
+                    <p>
+                        Lorem <strong>ipsum</strong> dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor <a href="https://mitchie.netlify.app/">incididunt</a> 
+                        ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute 
+                        irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat <strong>cupidatat non proident</strong>, 
+                        sunt in culpa qui officia deserunt mollit anim id est laborum.
+                    </p>
+                </div>
+			</div>
+		</footer>
+    )
+}
