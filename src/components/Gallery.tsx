5f9e006d92f2bcf9bcd19d0dde6459b91052c12d
Miroslava Hreško <miroslava.hresko@gmail.com>
2026-03-02 15:39:15 +0100

    Gallery update

--- src/components/Gallery.tsx
+++ src/components/Gallery.tsx
@@ -1,13 +1,14 @@
-import logo from '../assets/logo.svg'
+import styles from "./Gallery.module.css";
+import image1 from '../assets/gallery/1.jpg'
+import image2 from '../assets/gallery/2.jpg'
+import image3 from '../assets/gallery/3.jpg'
 
 function Gallery() {
   return (
-    <div>
-      <img src={logo} alt="logo" height="60px" width="auto" />
-      <img src={logo} alt="logo" height="60px" width="auto" />
-      <img src={logo} alt="logo" height="60px" width="auto" />
-      <img src={logo} alt="logo" height="60px" width="auto" />
-      <img src={logo} alt="logo" height="60px" width="auto" />
+    <div className={styles.gallery}>
+      <img src={image1} alt="logo" className={styles.image} />
+      <img src={image2} alt="logo" className={styles.image} />
+      <img src={image3} alt="logo" className={styles.image} />
     </div>
   )
 }
