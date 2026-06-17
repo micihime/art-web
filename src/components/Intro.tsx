99d88c30610b0ab3e94cd8635cfc6c5ecd522e07
Miroslava Hreško <miroslava.hresko@gmail.com>
2026-06-17 20:06:29 +0200

    Text polishing

--- src/components/Intro.tsx
+++ src/components/Intro.tsx
@@ -23,162 +23,95 @@ import gallery3img11 from "../assets/gallery/3/watercolor-b-1.jpg";
 import gallery3img12 from "../assets/gallery/3/watercolor-b-2.jpg";
 
 const gallery1 = [
-  {
-    src: gallery1img1,
-    alt: "Pretty scribble on sketchbook page",
-  },
-  {
-    src: gallery1img2,
-    alt: "Intentionally ugly scribble on sketchbook page",
-  },
+  { src: gallery1img1, alt: "Pretty scribble on sketchbook page" },
+  { src: gallery1img2, alt: "Intentionally ugly scribble on sketchbook page" },
 ];
 
 const gallery2 = [
-  {
-    src: gallery2img1,
-    alt: "Art supply test page 1",
-  },
-  {
-    src: gallery2img2,
-    alt: "Art supply test page 2",
-  },
-  {
-    src: gallery2img3,
-    alt: "Art supply test page 3",
-  },
-  {
-    src: gallery2img4,
-    alt: "Art supply test page 4",
-  },
+  { src: gallery2img1, alt: "Art supply test page 1" },
+  { src: gallery2img2, alt: "Art supply test page 2" },
+  { src: gallery2img3, alt: "Art supply test page 3" },
+  { src: gallery2img4, alt: "Art supply test page 4" },
 ];
 
 const gallery3 = [
-  {
-    src: gallery3img1,
-    alt: "Abstract sketch 1",
-  },
-  {
-    src: gallery3img2,
-    alt: "Abstract sketch 2",
-  },
-  {
-    src: gallery3img3,
-    alt: "Abstract sketch 3",
-  },
-  {
-    src: gallery3img4,
-    alt: "Abstract sketch 4 part 1",
-  },
-  {
-    src: gallery3img5,
-    alt: "Abstract sketch 4 part 2",
-  },
-  {
-    src: gallery3img6,
-    alt: "Abstract sketch 4 part 3",
-  },
-  {
-    src: gallery3img7,
-    alt: "Abstract sketch 5",
-  },
-  {
-    src: gallery3img8,
-    alt: "Abstract sketch 6",
-  },
-  {
-    src: gallery3img9,
-    alt: "Watercolor experiment A part 1",
-  },
-  {
-    src: gallery3img10,
-    alt: "Watercolor experiment A part 2",
-  },
-  {
-    src: gallery3img11,
-    alt: "Watercolor experiment B part 1",
-  },
-  {
-    src: gallery3img12,
-    alt: "Watercolor experiment B part 2",
-  },
+  { src: gallery3img1, alt: "Abstract sketch 1" },
+  { src: gallery3img2, alt: "Abstract sketch 2" },
+  { src: gallery3img3, alt: "Abstract sketch 3" },
+  { src: gallery3img4, alt: "Abstract sketch 4 part 1" },
+  { src: gallery3img5, alt: "Abstract sketch 4 part 2" },
+  { src: gallery3img6, alt: "Abstract sketch 4 part 3" },
+  { src: gallery3img7, alt: "Abstract sketch 5" },
+  { src: gallery3img8, alt: "Abstract sketch 6" },
+  { src: gallery3img9, alt: "Watercolor experiment A part 1" },
+  { src: gallery3img10, alt: "Watercolor experiment A part 2" },
+  { src: gallery3img11, alt: "Watercolor experiment B part 1" },
+  { src: gallery3img12, alt: "Watercolor experiment B part 2" },
 ];
 
 function Intro() {
   return (
     <section className={styles.intro}>
-      <p>
+      <p className={styles.lead}>
         I'm learning to paint and draw. Most of what you'll find here is a
-        record of that process: the experiments, the exercises, the sketches,
-        and the occasional successes.
+        living record of that process: the experiments, the exercises, the messy
+        sketches, and the occasional successes.
       </p>
+
       {/* self-portrait or photo of the workspace */}
+
       <h2>The Art of Intentional Ruin</h2>
-      <p>Earlier this year, I opened a brand-new sketchbook.</p>
-      <p>I'm a perfectionist. Or was? trying not to be.</p>
       <p>
-        I drew a random line on the very first page just to get the pressure
-        over with. I have a tendency to buy a lot of beautiful notebooks (I love
-        notebooks!)
+        Earlier this year, I opened a brand-new sketchbook. As a recovering
+        perfectionist, I knew exactly what would happen if I wasn't careful: I
+        would leave it completely blank. I have a chronic habit of buying
+        beautiful notebooks, only to leave them pristine out of fear that I'll
+        ruin them.
       </p>
-      <p>And then not use them - because I feel like I will ruin them.</p>
       <p>
-        So i wanted to intentionally ruin it on the first page. Draw an ugly
-        scribble. And with it, let the pressure to draw perfectly go.
+        To break the spell, I decided to intentionally ruin the first page. I
+        wanted to draw a completely ugly scribble and instantly banish the
+        pressure to be perfect. The problem? My first attempt came out way too
+        pretty. I had to flip to a second page and actively force myself to make
+        something genuinely hideous.
       </p>
-      <p>The problem? It came out too pretty.</p>
-      <p>
-        So on another page, I tried again and forced myself to make an
-        intentionally ugly scribble.
-      </p>
+
       <SimpleGallery images={gallery1} />
+
       <h2>Dusting Off the Shelf</h2>
       <p>
-        For years, I collected art supplies with the best intentions, only to
-        leave them sitting on my shelves.
+        I do the exact same thing with art supplies. At the store, I see a
+        beautiful set of pens or markers, and I immediately visualize the
+        masterpieces I'm going to create. <em>This time will be different,</em>{" "}
+        I tell myself. Instead, I just end up expanding my collection while
+        everything sits untouched on a shelf.
       </p>
       <p>
-        (The same thing I do with notebooks, I do with art supplies. Huh...)
+        When I finally committed to actually using them, the blank page was
+        paralyzing. I had no idea what to draw. To break the ice, I decided to
+        run a roll call for everything I'd been hoarding. I drew a few lines
+        with every single pen, pencil, marker, and wax crayon I owned. It wasn't
+        high art, but it got the ink flowing and forced me to actually use my
+        tools.
       </p>
-      <p>I get caught up in the idea that</p>
-      <p>
-        When I see some great new art supplies, I immediately see myself how I
-        do great artworks done with this tools. At the shop, I feel like 100%
-        this time it is going to be different. I will REALLY start
-        painting/drawing/journalling/etc.
-      </p>
-      <p>But I never did :D I was just expanding my collection.</p>
-      <p>
-        Then, when I finally decided that I will really do this, I did not know
-        what to draw. And it felt a little bit paralyzing. So I wondered, what
-        now?
-      </p>
-      <p>
-        I just needed to break the ice. So I decided, I will just draw some
-        lines with each and every one of my pencil and marker and wax pencil. To
-        just try out my tools that i bought over the years and never used and
-        they lay in my shelves unused.
-      </p>
-      <p>
-        My first pages became a roll call for everything I'd been hoarding: one
-        line for every pen, pencil, and marker I'd been saving.
-      </p>
-      <p>
-        This was like a starter idea to do something, at least something. And
-        just to use the stuff i bought in the past.
-      </p>
+
       <SimpleGallery images={gallery2} />
-      <h2>Finding a Style in the Chaos</h2>
+
+      <h2>Finding Style in the Chaos</h2>
       <p>
-        I've been trying different things: following prompts from Instagram,
-        testing out watercolor techniques from books, or making abstract pattern
-        sketches. In the name of combating perfectionism, I just try making it
-        up as I go and seeing what kind of textures happen, with no specific
-        plan beforehand.
+        Lately, I've been trying a bit of everything: following Instagram
+        prompts, practicing watercolor techniques from books, and losing myself
+        in abstract patterns. In the spirit of combating perfectionism, I try to
+        make things up as I go, focusing on textures and layers rather than a
+        rigid plan.
       </p>
+
       <SimpleGallery images={gallery3} />
+
       <p>
-        There is still a lot on my list to try. For now, this is simply a record
-        of what happens when I decide to keep showing up anyway.
+        There is still a massive list of things I want to try. For now, this
+        space is simply a record of what happens when I decide to get out of my
+        own way and just keep showing up.
       </p>
     </section>
   );
